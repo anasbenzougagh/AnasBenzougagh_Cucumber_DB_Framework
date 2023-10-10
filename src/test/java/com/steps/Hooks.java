81611353c3f109d65aeeb6e53d02e09d17eecea1
Anas_Cydeo <anasbenzougagh@gmail.com>
2023-10-09 17:09:38 -0700

    Initial commit

---
+++ src/test/java/com/steps/Hooks.java
@@ -0,0 +1,28 @@
+package steps;
+
+
+
+
+import com.library.utility.ConfigurationReader;
+import com.library.utility.DB_Util;
+import com.library.utility.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import utility.DB_Util;
+
+
+public class Hooks {
+
+    @Before("@db")
+    public void setUpDB(){
+        System.out.println("Connecting to database...");
+        DB_Util.createConnection();
+    }
+
+    @After("@db")
+    public void tearDownDB(){
+        System.out.println("close database connection...");
+        DB_Util.destroy();
+    }
+
+}
